d86890c90de001854715776c15dee2149fcfd7cc
Sayaji911 <sayajichavan9999@gmail.com>
2021-09-21 02:06:06 +0530

    check

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
+posts = [{
+    'author': 'CoreyMS',
+    'title': 'Blog Post 1',
+    'content': 'This is is just to check jinja',
+    'date_posted': 'September 20 2021'
+
+},
+    {
+    'author': 'Sayaji',
+    'title': 'Blog Post 2',
+    'content': 'This is is just to check jinja again',
+    'date_posted': 'September 20 2021'
+}
+
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', { 'title': 'About'})
