5f9cfce0ca95e8103158835df665510adb5d54e6
Sayaji911 <sayajichavan9999@gmail.com>
2021-09-29 17:47:47 +0530

    added form validation.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [{
-    'author': 'User4',
-    'title': 'Blog Post 1',
-    'content': 'This is is just to check jinja',
-    'date_posted': 'September 20 2021'
-
-},
-    {
-    'author': 'Sayaji',
-    'title': 'Blog Post 2',
-    'content': 'This is is just to check jinja again',
-    'date_posted': 'September 20 2021'
-}
-
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
